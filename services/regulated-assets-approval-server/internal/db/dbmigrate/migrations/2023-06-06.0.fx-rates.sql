32aece5f2a6d48c9fb8f6da5bc1f49c8d3d6377e
Yuri E <yuriescl@gmail.com>
2023-06-07 08:57:42 -0300

    wip

--- services/regulated-assets-approval-server/internal/db/dbmigrate/migrations/2023-06-06.0.fx-rates.sql
+++ services/regulated-assets-approval-server/internal/db/dbmigrate/migrations/2023-06-06.0.fx-rates.sql
@@ -5,7 +5,7 @@ CREATE TABLE public.fx_rates (
     asset_code text NOT NULL,
     asset_issuer text NOT NULL,
     usd_rate text NOT NULL,  -- amount of asset that corresponds to 1 US Dollar
-    rate_timestamp timestamp with time zone NOT NULL DEFAULT NOW(),
+    rate_timestamp timestamp with time zone NOT NULL DEFAULT NOW()
 );
 
 -- +migrate Down
